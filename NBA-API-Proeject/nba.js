9a7a4f1778aeb04878aeb0703825bdd326a03515
alibaltaci <alibaltaci13@gmail.com>
2022-03-30 13:11:20 +0300

    footer field edited

--- NBA-API-Proeject/nba.js
+++ NBA-API-Proeject/nba.js
@@ -1,7 +1,7 @@
 // With ES6 ()
 
 
-const BASE_URL = "https://free-nba.p.rapidapi.com"
+const BASE_URL = "https://free-nba.p.rapidapi.com/players"
 
 const fetchConfig = {
     "headers": {"x-rapidapi-key": "d69cb5bd09msha8f3eb3e66141c2p1c38eejsncf3170289181"}
@@ -11,7 +11,7 @@ class NBA{
 
     getPlayers(player){
         return new Promise( (resolve, reject) => {
-            fetch(`${BASE_URL}/players?page=0&per_page=25&search=${player}`, fetchConfig)
+            fetch(`${BASE_URL}?page=0&per_page=25&search=${player}`, fetchConfig)
             .then(response => response.json())
             .then(players => resolve(players))
             .catch(err => reject(err));
@@ -20,7 +20,7 @@ class NBA{
 
     getPlayersWithID(player){
         return new Promise( (resolve, reject) => {
-            fetch(`${BASE_URL}/players/${player}`, fetchConfig)
+            fetch(`${BASE_URL}/${player}`, fetchConfig)
             .then(response => response.json())
             .then(players => resolve(players))
             .catch(err => reject(err));
