0ffcef131cd3829b6f3773d9d8ed6700021cd81f
alibaltaci <alibaltaci13@gmail.com>
2022-04-01 01:13:59 +0300

    setTimeout - setInterval - clearInputs

--- Love-Calculator-API-Project/ui.js
+++ Love-Calculator-API-Project/ui.js
@@ -6,20 +6,58 @@ class UI{
 
         this.resultMessage = document.getElementById("result-message");
 
+        this.header = document.getElementById("resultsHeader");
+
+        this.nameFirst = document.getElementById("couple-name-first");
+
+        this.nameSecond = document.getElementById("couple-name-second");
+
     }
 
     addResultsToUI(response){
 
-        this.resultPer.innerHTML = `
+        if(this.resultMessage.firstElementChild !== null){
+            this.resultMessage.removeChild(this.resultMessage.firstElementChild);
+        }
 
-        <div id="persentage-box"> <span>${response.percentage}</span> % </div>
+        this.header.innerHTML = `<h1>${response.sname} & ${response.fname}`;
+
+        let per = response.percentage;
+
+        let counter = 0;
+
+        setInterval( () => {
+
+            if(counter <= per){
+            
+                this.resultPer.innerHTML = `
+
+                <div id="percentage-box"> <span>${counter}</span> % </div>
         
-        `;
+                `;
+            }
 
-        this.resultMessage.innerHTML = `
+            counter++
+            
+        },100);
+
+        setTimeout( () => 
+
+            {this.resultMessage.innerHTML = `
         
-        <div id="message-box"> ${response.result} </div>
+            <div id="message-box"> ${response.result} </div>
+    
+            `;}, per * 100)
 
-        `;
+
+
+    }
+
+    clearInputs(){
+
+        this.nameFirst.value = "";
+
+        this.nameSecond.value = "";
+
     }
 }
