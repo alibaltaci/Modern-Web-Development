ff0a846ce0963445edb10c8344a9c79847fc7392
alibaltaci <alibaltaci13@gmail.com>
2022-03-31 15:37:58 +0300

    modifed

--- Love-Calculator-API-Project/love.js
+++ Love-Calculator-API-Project/love.js
@@ -1,5 +1,5 @@
 class LOVE{
-    getResult(){
+    getResult(coupleNameFirst, coupleNameSecond){
         return new Promise( (resolve, reject) => {     
             fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${coupleNameFirst}&fname=${coupleNameSecond}`, {
                 "headers": {"x-rapidapi-key": "d69cb5bd09msha8f3eb3e66141c2p1c38eejsncf3170289181"}
